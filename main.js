34fa3e938b2a0e764f9586e4e8151197a6cc31c1
Tomaáš Figura <Figura>
2022-05-25 14:03:02 +0200

    Main

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import RomanNumber from './RomanNumber.js';
+import assert from 'node:assert';
 
-const romanNumber = new RomanNumber("IVXLCDM");
-//console.log(romanNumber.romanLetters)
+const romanNumber = new RomanNumber("IVXLCDMSAC");
+
+assert.equal(romanNumber.romanLetters, "IVXLCDM", "My message goes here");
